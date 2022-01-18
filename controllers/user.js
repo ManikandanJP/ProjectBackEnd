f9ca794fe8fc7ce68bbafa55600e1bb1b98924e2
Manikandan Jayaprakash <manikak2002@gmail.com>
2022-01-19 01:13:50 +0530

    all users

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,11 @@
+const user = require("../models/user");
 const User = require("../models/user");
 
 exports.getUserById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
     if (err || !user) {
       return res.status(400).json({
-        error: "No user was found in DB"
+        error: "No user was found in DB",
       });
     }
     req.profile = user;
@@ -13,7 +14,18 @@ exports.getUserById = (req, res, next, id) => {
 };
 
 exports.getUser = (req, res) => {
- // req.profile.salt = undefined;
- // req.profile.encry_password = undefined;
+  req.profile.salt = undefined;
+  req.profile.encry_password = undefined;
   return res.json(req.profile);
 };
+
+exports.getAllUsers = (req , res) => {
+    User.find().exec((err, users)=>{
+        if( err || !users) {
+            return res.status(400).json({
+                error :" No Users Found in DB"
+            })
+        }
+        res.json(users);
+    });
+}
